52231d4a9e7b58501b72de04d7a47cf72e9a8f5f
bagasadit123 <bagasadit430@gmail.com>
2026-04-21 11:34:10 +0700

    menambahkan bool search dan listempty

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -56,4 +56,26 @@ public:
         nodeBaru->next = current;
         previous->next = nodeBaru;
     }
+    
+    bool listempty()
+    {
+        return (START == NULL);
+    }
+     bool listempty()
+    {
+        return (START == NULL);
+    }
+
+    bool search(int nim, Node *&previous, Node *&current)
+{
+    previous = START;
+    current = START;
+
+    while (current != NULL && nim != current->noMhs)
+    {
+        previous = current;
+        current = current->next;
+    }
+
+    return (current != NULL);
 }
