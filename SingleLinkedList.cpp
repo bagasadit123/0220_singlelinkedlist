d129c3b189300e83976c2a2a6fc1e93f4f1bdd28
bagasadit123 <bagasadit430@gmail.com>
2026-04-21 11:19:19 +0700

    menambahkan node

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -17,4 +17,27 @@ public:
     {
         START = NULL;
     }
-};
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diizinkan\n";
+                return;
+            }
+
+            nodeBaru->next =START;
+            START = nodeBaru;
+            return;
+        }
+    }
+}
